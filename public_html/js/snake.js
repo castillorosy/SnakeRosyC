a59e43adb2a42f7f91fb5d307113dd58ec45428d
castillorosy <rosycastillo18@yahoo.com>
2014-10-03 13:45:05 -0700

    first commit.

---
+++ public_html/js/snake.js
@@ -0,0 +1,17 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight; 
+
+function gameInitialized() {
+    var canvas = document.getElementById("game-screen");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
