ce8f83bf7b4604a9fdf492e102f8cb9cc017f38c
castillorosy <rosycastillo18@yahoo.com>
2014-10-21 13:59:42 -0700

    my stuff restarted Dx

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -98,17 +98,27 @@ function foodInitialized() {
 
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y *  snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
+/*
+ * Input Function
+ */
 
 function keyboardHandler(event) {
     console.log(event);
+    
+    if(event.keyCode === "39") {
+        snakeDirection = "right";
+    }
+    else if(event.keyCode ==="40") {
+        snakeDirection = "down";
+    }
 }
