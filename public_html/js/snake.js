790e4d29809a109139212b5320350d5e2fc07631
castillorosy <rosycastillo18@yahoo.com>
2014-10-24 14:03:13 -0700

    idk

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -13,10 +13,12 @@ var context;
 var screenWidth;
 var screenHeight;
 
+var gameState;
+
 gameInitialized();
 snakeInitialized();
 foodInitialized();
-setInterval(gameLoop, 1000/30);
+setInterval(gameLoop, 1000 / 30);
 
 /*
  * creating the snake and putting the height
@@ -31,15 +33,19 @@ function gameInitialized() {
 
     canvas.width = screenWidth;
     canvas.height = screenHeight;
-    
+
     document.addEventListener("keydown", keyboardHandler);
+
+    setState("PLAY");
 }
 
 function gameLoop() {
     gameDraw();
-    snakeUpdate();
-    snakeDraw();
-    foodDraw();
+    if (gameState == "PLAY") {
+        snakeUpdate();
+        snakeDraw();
+        foodDraw();
+    }
 }
 
 function gameDraw() {
@@ -51,7 +57,7 @@ function snakeInitialized() {
     snake = [];
     snakeLength = 1;
     snakeSize = 20;
-    snakeDirection = "down"; 
+    snakeDirection = "down";
 
     for (var index = snakeLength - 1; index >= 0; index--) {
         snake.push({
@@ -70,25 +76,25 @@ function snakeDraw() {
 function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
-    
+
     checkFoodCollisions(snakeHeadX, snakeHeadY);
     checkWallCollisions(snakeHeadX, snakeHeadY);
 
-    if(snakeDirection == "down") {
-      snakeHeadY++;  
+    if (snakeDirection == "down") {
+        snakeHeadY++;
     }
-    
-    else if(snakeDirection == "right") {
+
+    else if (snakeDirection == "right") {
         snakeHeadX++;
     }
-    if(snakeDirection == "up") {
+    if (snakeDirection == "up") {
         snakeHeadY--;
     }
-    else if(snakeDirection == "left") {
+    else if (snakeDirection == "left") {
         snakeHeadX--;
     }
-    
-        
+
+
 
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
@@ -103,7 +109,7 @@ function snakeUpdate() {
 function foodInitialized() {
     food = {
         x: 0,
-        y: 0 
+        y: 0
     };
     setFoodPosition();
 }
@@ -116,7 +122,7 @@ function foodDraw() {
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
-    
+
     food.x = Math.floor(randomX / snakeSize);
     food.y = Math.floor(randomY / snakeSize);
 }
@@ -126,20 +132,20 @@ function setFoodPosition() {
 
 function keyboardHandler(event) {
     console.log(event);
-    
-    if(event.keyCode == "39" && snakeDirection != "left") {
+
+    if (event.keyCode == "39" && snakeDirection != "left") {
         snakeDirection = "right";
     }
-    else if(event.keyCode == "40" && snakeDirection != "up") {
+    else if (event.keyCode == "40" && snakeDirection != "up") {
         snakeDirection = "down";
     }
-    if(event.keyCode == "38" && snakeDirection != "down") {
+    if (event.keyCode == "38" && snakeDirection != "down") {
         snakeDirection = "up";
     }
-    else if(event.keyCode == "37" && snakeDirection != "right") {
+    else if (event.keyCode == "37" && snakeDirection != "right") {
         snakeDirection = "left";
     }
-}   
+}
 /*
  * collision Handling
  * "snakeHeadX == food.x" is checking wiether or not the snakeHeadX
@@ -147,17 +153,25 @@ function keyboardHandler(event) {
  */
 
 function checkFoodCollisions(snakeHeadX, snakeHeadY) {
-   if(snakeHeadX == food.x && snakeHeadY == food.y) {
-       snake.push({
-           x:0,
-           y:0
-       });
-       snakeLength++;
-   } 
+    if (snakeHeadX == food.x && snakeHeadY == food.y) {
+        snake.push({
+            x: 0,
+            y: 0
+        });
+        snakeLength++;
+    }
 }
 
 function checkWallCollisions(snakeHeadX, snakeHeadY) {
-    if(snakeHeadX * snakeSize >= screenWidth || snakeHeadX * snakeSize< 0) {
-        console.log("Wall Collision");
+    if (snakeHeadX * snakeSize >= screenWidth || snakeHeadX * snakeSize < 0) {
+        setState("GAME OVER");
     }
+}
+/*
+ * game state heading
+ * 
+ */
+
+function setState(state) {
+    gameState = state;
 }
