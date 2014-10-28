a2bbe88f995fb4ac4f3aff7befb538ac3b59edbf
castillorosy <rosycastillo18@yahoo.com>
2014-10-28 14:02:37 -0700

    starting video 50

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -36,8 +36,9 @@ function gameInitialized() {
     canvas.height = screenHeight;
 
     document.addEventListener("keydown", keyboardHandler);
-    
+
     gameOverMenu = document.getElementById("gameOver");
+    centerMenuPosition(gameOverMenu);
 
     setState("PLAY");
 }
@@ -180,12 +181,21 @@ function setState(state) {
     showMenu(state);
 }
 
+/*
+ * menu functions
+ */
+
 function displayMenu(menu) {
     menu.style.visibility = "visible";
 }
 
 function showMenu(state) {
-    if(state == "GAME OVER") {
+    if (state == "GAME OVER") {
         displayMenu(gameOverMenu);
     }
+}
+
+ function centerMenuPosition(menu) {
+    menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2)+ "px";
+    menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2)+ "px";
 }
